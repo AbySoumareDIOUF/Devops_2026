d780c76b3c391fa898167e0cd0585c904d6096bd
AbySoumareDIOUF <dioufabibatou1804@gmail.com>
2026-02-07 12:23:04 +0000

    First commit

---
+++ src/main/java/uahb/devops/githubDevops/GithubDevopsApplication.java
@@ -0,0 +1,13 @@
+package uahb.devops.githubDevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubDevopsApplication.class, args);
+	}
+
+}
